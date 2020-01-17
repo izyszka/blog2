eeb3709ae05124907e1a099d8048bd48db2f408c
izyszka <iza.wisniewska.iza@gmail.com>
2020-01-17 21:55:26 +0100

    Create post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,18 @@
 class PostsController < ApplicationController
+  
+  def index
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :body))
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
 end
